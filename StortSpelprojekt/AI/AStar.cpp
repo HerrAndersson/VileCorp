7dd8041a64b91f7ca3cc2b0f1cbb9192d0c5ab65
vima13 <victorarias@live.se>
2015-11-25 12:09:30 +0100

    ADDED A* pathfinding

---
+++ StortSpelprojekt/AI/AStar.cpp
@@ -0,0 +1,193 @@
+#include "AStar.h"
+
+namespace AI
+{
+	bool AStar::IsPositionValid(Vec2D pos)
+	{
+		return pos._x >= 0 && pos._x < _width && pos._y >= 0 && pos._y < _height;
+	}
+
+	//Calculates h based on the distance to the goal
+	void AStar::CalculateHCost(Vec2D pos)
+	{
+		__int16 a = abs(_goal._x - pos._x);						//horizontal distance to goal
+		__int16 b = abs(_goal._y - pos._y);						//vertical distance to goal
+		switch (_heuristicType)
+		{
+		case AStar::MANHATTAN:
+			_grid[pos._x][pos._y]._hCost = a + b;
+			break;
+		case AStar::CHEBYSHEV:
+			_grid[pos._x][pos._y]._hCost = std::min(a, b) + abs(a - b);
+			break;
+		case AStar::OCTILE:
+			_grid[pos._x][pos._y]._hCost = SQRT2 * std::min(a, b) + abs(a - b);
+			break;
+		case AStar::EUCLIDEAN:
+			_grid[pos._x][pos._y]._hCost = sqrt(a*a + b*b);
+			break;
+		default:
+			break;
+		}
+		_grid[pos._x][pos._y]._hCost *= _hWeight;
+	}
+
+	//calculates g by adding the preceding nodes g-cost to the current tilecost.
+	void AStar::CalculateGCost(Vec2D parentPos, Vec2D currentPos)
+	{
+		float g = 0.0f;
+		//Euclidean heuristic is locked to octile movement for now.
+		switch (_heuristicType)
+		{
+		case AStar::MANHATTAN:
+		case AStar::CHEBYSHEV:
+			g = _grid[parentPos._x][parentPos._y]._gCost + _grid[currentPos._x][currentPos._y]._tileCost;
+			break;
+		case AStar::OCTILE:
+		case AStar::EUCLIDEAN:
+			if (parentPos._x == currentPos._x || parentPos._y == currentPos._y)
+			{
+				g = _grid[parentPos._x][parentPos._y]._gCost + _grid[currentPos._x][currentPos._y]._tileCost;
+			}
+			else
+			{
+				g = _grid[parentPos._x][parentPos._y]._gCost + SQRT2 * _grid[currentPos._x][currentPos._y]._tileCost;
+			}
+			break;
+		default:
+			break;
+		}
+
+		// tileCost < 0 means unwalkable. open == 0 means no previous gCost, meaning g is automatically better.
+		if (_grid[currentPos._x][currentPos._y]._tileCost > 0 && (_grid[currentPos._x][currentPos._y]._open == 0 || _grid[currentPos._x][currentPos._y]._gCost > g))
+		{
+			_grid[currentPos._x][currentPos._y]._open = 1;
+			_grid[currentPos._x][currentPos._y]._gCost = g;
+			_grid[currentPos._x][currentPos._y]._parent = &_grid[parentPos._x][parentPos._y];
+			_openQueue.insert(_grid[currentPos._x][currentPos._y]);										//insert should logically fit outside the function, but it works better with the if-check here.
+		}
+	}
+
+	AStar::AStar()
+	{
+		_pathLength = 0;
+		_path = nullptr;
+		_width = 0;
+		_height = 0;
+		_start = {0,0};
+		_goal = {0,0};
+		_heuristicType = MANHATTAN;
+		_hWeight = 0;
+		_openQueue = Heap<Node>();
+		_grid = nullptr;
+	}
+
+	AStar::AStar(int width, int height, Vec2D start, Vec2D goal, Heuristic heuristic, int hWeight)
+	{
+		_pathLength = 0;
+		_path = nullptr;
+		_width = width;
+		_height = height;
+		_start = start;
+		_goal = goal;
+		_heuristicType = heuristic;
+		_hWeight = hWeight;
+		_openQueue = Heap<Node>();
+		_grid = new Node*[_width];
+		for (__int16 i = 0; i < _width; i++)
+		{
+			_grid[i] = new Node[_height];
+			for (__int16 j = 0; j < _height; j++)
+			{
+				_grid[i][j] = Node(i, j);
+				_grid[i][j]._open = 0;
+				//CalculateHCost({i,j});
+			}
+		}
+	}
+
+	AStar::~AStar()
+	{
+		for (__int16 i = 0; i < _width; i++)
+		{
+			delete[] _grid[i];
+		}
+		delete[] _grid;
+		delete[] _path;
+	}
+
+	void AStar::SetTileCost(Vec2D pos, int cost)
+	{
+		_grid[pos._x][pos._y]._tileCost = cost;
+	}
+
+	void AStar::SetStartPosition(Vec2D pos)
+	{
+		_start = pos;
+	}
+
+	Vec2D * AStar::GetPath() const
+	{
+		return _path;
+	}
+
+	int AStar::GetPathLength() const
+	{
+		return _pathLength;
+	}
+
+	void AStar::cleanMap()
+	{
+		for (__int16 i = 0; i < _width; i++)
+		{
+			_grid[i] = new Node[_height];
+			for (__int16 j = 0; j < _height; j++)
+			{
+				_grid[i][j]._open = 0;
+			}
+		}
+		_openQueue.~Heap();
+		_openQueue = Heap<Node>();
+	}
+
+	/*
+	Runs the actual A* algorithm.
+	TODO: Return the path as a sorted list
+	--Victor
+	*/
+	void AStar::FindPath()
+	{
+		_pathLength = 1;																//It's 1 because there's an offset in the loop later.
+		Vec2D currentPos = _start;
+		_grid[_start._x][_start._y]._open = 2;
+
+		while (currentPos != _goal)														//loops until a path has been found
+		{
+			for (int i = 0; i < 8 && (_heuristicType != MANHATTAN || i < 4); i++)		//Manhattan skips diagonals 
+			{
+				Vec2D checkedPos = currentPos + NEIGHBOUR_OFFSETS[i];
+				if (IsPositionValid(checkedPos) && _grid[checkedPos._x][checkedPos._y]._open != 2 &&											//checks for borders and already visited
+					_grid[checkedPos._x][currentPos._y]._tileCost >= 0 && _grid[currentPos._x][checkedPos._y]._tileCost >= 0)					//checks for corners
+				{
+					CalculateHCost(currentPos);											//As the program works now, h must be calculated before g.
+					CalculateGCost(currentPos, checkedPos);
+				}
+			}
+			currentPos = _openQueue.removeMin()._position;
+			_grid[currentPos._x][currentPos._y]._open = 2;
+		}
+		while (currentPos != _start)													//traces the route back to start
+		{
+			_pathLength++;
+			currentPos = _grid[currentPos._x][currentPos._y]._parent->_position;
+		}
+		_path = new Vec2D[_pathLength];
+		int c = 0;
+		currentPos = _goal;
+		while (currentPos != _start)													//traces the route back to start
+		{
+			_path[c++] = currentPos;
+			currentPos = _grid[currentPos._x][currentPos._y]._parent->_position;
+		}
+	}
+}
---
+++ StortSpelprojekt/AI/AStar.h
@@ -0,0 +1,134 @@
+#pragma once
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "Heap.h"
+
+#define AI_EXPORT __declspec(dllexport)
+
+namespace AI
+{
+
+	struct AI_EXPORT Vec2D
+	{
+		short _x, _y;
+		Vec2D()
+		{
+			_x = 0;
+			_y = 0;
+		}
+		Vec2D(int x, int y)
+		{
+			_x = x;
+			_y = y;
+		}
+		bool operator==(const Vec2D& comp)
+		{
+			return _x == comp._x && _y == comp._y;
+		}
+		bool operator!=(const Vec2D& comp)
+		{
+			return _x != comp._x || _y != comp._y;
+		}
+		Vec2D operator+(const Vec2D& comp)
+		{
+			return Vec2D{_x + comp._x, _y + comp._y};
+		}
+		Vec2D operator-(const Vec2D& comp)
+		{
+			return Vec2D{_x - comp._x, _y - comp._y};
+		}
+		void operator=(const Vec2D& comp)
+		{
+			_x = comp._x;
+			_y = comp._y;
+		}
+	};
+
+	/*
+		Basic A* algorithm operating on a map with varying tile cost.
+		Should be relatively easy to expand for more specialized behavior
+		--Victor
+	*/
+	class AI_EXPORT AStar
+	{
+	public:
+		const double SQRT2 = 1.41421356;								//used for diagonals. Placing a constant means no root-calculations.
+		const Vec2D NEIGHBOUR_OFFSETS[8] = {{-1, 0},{1, 0},{0, -1},{0, 1},{-1, -1},{1, -1},{-1, 1},{1, 1}};	//Straight moves in 0-3, diagonal in 4-7
+
+		/*
+			Different heuristic used for estimating the distance to the goal
+			MANHATTAN: No diagonal movement
+			CHEBYSHEV: Diagonal movement has a cost of 1
+			OCTILE: Diagonal movement has a cost of ?2
+			EUCLIDEAN: Calculates the distance in a straight line to the goal.
+		*/
+		enum Heuristic
+		{
+			MANHATTAN, CHEBYSHEV, OCTILE, EUCLIDEAN
+		};
+	private:
+		struct Node
+		{
+			Vec2D _position;
+			__int8 _open;										//0 = not checked, 1 = open, 2 = closed
+			__int8 _tileCost;									//cost of traversing the individual tile
+			float _gCost, _hCost;								//distance from start and heuristic to goal, respectively
+			Node* _parent;										//the path back to the start node 
+			Node()
+			{
+				_position = {0, 0};
+				_hCost = -1.0f;
+				_gCost = -1.0f;
+				_open = 0;
+			}
+			Node(__int16 x, __int16 y, float h = -1.0f)
+			{
+				_position = {x, y};
+				_hCost = h;
+				_gCost = -1.0f;
+				_open = 0;
+			}
+			~Node()
+			{}
+			bool operator<(const Node& comp)
+			{
+				return (_gCost + _hCost) < (comp._gCost + comp._hCost);
+			}
+			bool operator>(const Node& comp)
+			{
+				return (_gCost + _hCost) > (comp._gCost + comp._hCost);
+			}
+			void operator=(const Node& comp)
+			{
+				_position = comp._position;
+				_gCost = comp._gCost;
+				_hCost = comp._hCost;
+				_parent = comp._parent;
+			}
+		};
+		int _pathLength;
+		Vec2D* _path;											//An ordered array moving from goal to start
+		__int16 _height, _width;
+		Node** _grid;
+	//	std::vector<Node> _openQueue;							//A priority queue for open nodes
+		Heap<Node> _openQueue;
+		Vec2D _start, _goal;
+		Heuristic _heuristicType;
+		__int8 _hWeight;										//heuristic weight for moving a tile
+		bool IsPositionValid(Vec2D pos);
+		void CalculateHCost(Vec2D pos);
+		void CalculateGCost(Vec2D parentPos, Vec2D currentPos);
+	public:
+		AStar();
+		AStar(int width, int height, Vec2D start, Vec2D goal, Heuristic heuristic = MANHATTAN, int hWeight = 1);
+		virtual ~AStar();
+		void SetTileCost(Vec2D pos, int cost = 1);
+		void SetStartPosition(Vec2D pos);
+		Vec2D* GetPath() const;
+		int GetPathLength() const;
+		void cleanMap();
+		void FindPath();
+	};
+
+}
