befe8fd352ff6d6730a07a3fc689f291f015d495
vima13 <victorarias@live.se>
2015-12-01 14:17:33 +0100

    ADDED all the things
    (Things referring to various GameObject subclasses)

--- StortSpelprojekt/StortSpelprojekt/Unit.cpp
+++ StortSpelprojekt/StortSpelprojekt/Unit.cpp
@@ -9,9 +9,9 @@ Unit::Unit()
 
 Unit::Unit(int x, int z, const Tilemap* tileMap)
 {
-	_unitTilePosition = {x, z};
+	//_unitTilePosition = {x, z};
 	_tileMap = tileMap;
-	_aStar = new AI::AStar(_tileMap->GetWidth(), _tileMap->GetHeight(), _unitTilePosition, {0,0}, AI::AStar::OCTILE);		//TODO: Find the unit's goal --Victor
+	_aStar = new AI::AStar(_tileMap->GetWidth(), _tileMap->GetHeight(), {x, z}, {0,0}, AI::AStar::OCTILE);		//TODO: Find the unit's goal --Victor
 
 	for (int i = 0; i < _tileMap->GetWidth(); i++)
 	{
@@ -36,20 +36,58 @@ Unit::Unit(int x, int z, const Tilemap* tileMap)
 	_pathLength = _aStar->GetPathLength();
 }
 
+Unit::Unit(unsigned short ID, DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 rotation, AI::Vec2D tilePosition, Type type, RenderObject* renderObject, const Tilemap* tileMap)
+	: GameObject(ID, position, rotation, tilePosition, type, renderObject)
+{
+	_tileMap = tileMap;
+	_aStar = new AI::AStar(_tileMap->GetWidth(), _tileMap->GetHeight(), _tilePosition, {0,0}, AI::AStar::OCTILE);		//TODO: Find the unit's goal --Victor
+
+	//Scan tilemap for floor layout and objectives
+	for (int i = 0; i < _tileMap->GetWidth(); i++)
+	{
+		for (int j = 0; j < _tileMap->GetHeight(); j++)
+		{
+			if (_tileMap->IsObjectiveOnTile(i, j))
+			{
+				_aStar->SetGoalPosition({i, j});
+			}
+			if (_tileMap->IsWallOnTile(i, j))
+			{
+				_aStar->SetTileCost({i, j}, -1);						//TODO: More thorough check depending on objects in the tile --Victor
+			}
+			else
+			{
+				_aStar->SetTileCost({i, j}, 1);
+			}
+		}
+	}
+	_aStar->FindPath();
+	_path = _aStar->GetPath();
+	_pathLength = _aStar->GetPathLength();
+}
+
 
 Unit::~Unit()
 {
 	delete _aStar;
 }
 
+/*
+	Moves the unit to the tile it's aimning for and selects a new walking direction.
+	This should NOT update every frame. It only updates when the unit reaches a new tile. 
+*/
 void Unit::Move()
 {
-	_unitTilePosition = _path[--_pathLength];				//Temporary. Units shouldn't move one tile per frame.
+	_tilePosition += _direction;
+
+	AI::Vec2D nextTile = _path[--_pathLength];
+	_direction = {nextTile._x, nextTile._y};
+	_direction -= _tilePosition;
 }
 
 void Unit::Update()
 {
-	Move();
+	//Move();
 	//TODO: update map within line of sight.
 }
 
--- StortSpelprojekt/StortSpelprojekt/Unit.h
+++ StortSpelprojekt/StortSpelprojekt/Unit.h
@@ -6,17 +6,17 @@ class Unit : public GameObject
 {
 private:
 	AI::AStar* _aStar;
-	AI::Vec2D _unitTilePosition;
+	//AI::Vec2D _unitTilePosition;				//replaced by _tilePosition in GameObject
 	AI::Vec2D _goalTilePosition;
 	AI::Vec2D* _path;
 	int _pathLength;
 	const Tilemap* _tileMap;		//Pointer to the tileMap in objectHandler(?). Units should preferably have read-, but not write-access.
-
+	AI::Vec2D _direction;
 	float  _moveSpeed;
 public:
 	Unit();
 	Unit(int x, int z, const Tilemap* tileMap);
-	Unit(unsigned short ID, Vec3 position, Type type, RenderObject * renderObject);
+	Unit(unsigned short ID, DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 rotation, AI::Vec2D tilePosition, Type type, RenderObject* renderObject, const Tilemap* tileMap);
 	~Unit();
 	void Move();
 	void Update();
