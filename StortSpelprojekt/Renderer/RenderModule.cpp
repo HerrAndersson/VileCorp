721754be45570a7f0830a0d72f7801df1f2231b2
FredrikLindsten <ehh.va@hotmail.com>
2015-12-08 16:18:49 +0100

    FIXED default skeleton if in RenderModule.cpp

--- StortSpelprojekt/Renderer/RenderModule.cpp
+++ StortSpelprojekt/Renderer/RenderModule.cpp
@@ -216,7 +216,7 @@ namespace Renderer
 
 		int vertexSize = sizeof(Vertex);
 
-		if (renderObject->_skeleton != -1)
+		if (renderObject->_skeleton != 0)
 		{
 			vertexSize = sizeof(WeightedVertex);
 		}
