c9da8f73eea46a52496384cbe5731dc86e9b3c1e
lunderot <matriz@live.se>
2016-02-16 15:55:39 +0100

    REMOVED unnecessary code in MenuState constructor

--- StortSpelprojekt/StortSpelprojekt/StateMachine/MenuState.cpp
+++ StortSpelprojekt/StortSpelprojekt/StateMachine/MenuState.cpp
@@ -3,13 +3,7 @@
 MenuState::MenuState(System::Controls* controls, ObjectHandler* objectHandler, System::Camera* camera, PickingDevice* pickingDevice, const std::string& filename, AssetManager* assetManager, FontWrapper* fontWrapper, System::Settings* settings, System::SoundModule* soundModule)
 	: BaseState (controls, objectHandler, camera, pickingDevice, filename, "MENU", assetManager, fontWrapper, settings, soundModule)
 {
-	_controls = controls;
-	_objectHandler = objectHandler;
-	
-	_camera = camera;
-	_pickingDevice = pickingDevice;
-
-	_soundModule->AddSound("Assets/Sounds/page.wav",1.0f, 1.0f, true, false);
+	_soundModule->AddSound("Assets/Sounds/page.wav", 1.0f, 1.0f, true, false);
 }
 
 MenuState::~MenuState()
