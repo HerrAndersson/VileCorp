b2ee67a1e92ebfbdea6ae272b70e679d111d93a6
BlunderChan <seb.lqv@gmail.com>
2016-02-16 11:58:14 +0100

    FIXED camera now zooming in the direction of its forward vector.
    CHANGED Camera to be more general by changing function Move to use velocity instead of deltaTime.

--- StortSpelprojekt/StortSpelprojekt/GameLogic.cpp
+++ StortSpelprojekt/StortSpelprojekt/GameLogic.cpp
@@ -162,17 +162,26 @@ void GameLogic::HandleInput(float deltaTime)
 	/*
 	Camera scroll
 	*/
+	const float ZOOM_MAX = 12.0f;
+	const float ZOOM_MIN = 4.0f;
+	float velocity = deltaTime / 20.0f;
+
 	if (_camera->GetMode() == System::LOCKED_CAM)
 	{
 		if (_controls->IsFunctionKeyDown("CAMERA:ZOOM_CAMERA_IN") &&
-			_camera->GetPosition().y > 4.0f)
+			(_camera->GetPosition().y - velocity) > ZOOM_MIN)
 		{
-			_camera->Move(XMFLOAT3(0.0f, -1.0f, 0.0f), deltaTime);
+			_camera->Move(_camera->GetForwardVector(), velocity);
 		}
 		else if (_controls->IsFunctionKeyDown("CAMERA:ZOOM_CAMERA_OUT") &&
-			_camera->GetPosition().y < 12.0f)
+			(_camera->GetPosition().y + velocity) < ZOOM_MAX)
 		{
-			_camera->Move(XMFLOAT3(0.0f, 1.0f, 0.0f), deltaTime);
+			XMFLOAT3 negForward = XMFLOAT3(
+				_camera->GetForwardVector().x * -1,
+				_camera->GetForwardVector().y * -1,
+				_camera->GetForwardVector().z * -1);
+
+			_camera->Move( negForward, velocity);
 		}
 	}
 
@@ -268,7 +277,7 @@ void GameLogic::HandleInput(float deltaTime)
 
 	if (isMoving)
 	{
-		_camera->Move(XMFLOAT3((forward.x + right.x) * v, (forward.y + right.y) * v, (forward.z + right.z) * v), deltaTime);
+		_camera->Move(XMFLOAT3((forward.x + right.x) * v, (forward.y + right.y) * v, (forward.z + right.z) * v), deltaTime / 10);
 	}
 	
 }
