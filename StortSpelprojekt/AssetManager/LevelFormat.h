555d2bbdbdb8091605e5379d681b9acc1022eeff
rikhardsvensson <rille88@gmail.com>
2016-02-17 14:13:33 +0100

    CHANGED Updated LevelFormat to reflect the documented changes.

--- StortSpelprojekt/AssetManager/LevelFormat.h
+++ StortSpelprojekt/AssetManager/LevelFormat.h
@@ -9,17 +9,23 @@
 
 struct LevelHeader
 {
-	std::string _description = "";
+	std::string _storyTitle = "";
+	std::string _storyBody = "";
 	int _budget = 0;
+	int _gameMode = 0;
+	int _surviveForSeconds = 0; //Specifically for the survival gamemode 
 	std::string _levelBinaryFilename = "";
 
 	template<class A>
 	void serialize(A& a)
 	{
 		a(
-			CEREAL_NVP(_description),
+			CEREAL_NVP(_storyTitle),
+			CEREAL_NVP(_storyBody),
 			CEREAL_NVP(_budget),
-			CEREAL_NVP(_levelBinaryFilename)
+			CEREAL_NVP(_storyBody),
+			CEREAL_NVP(_gameMode),
+			CEREAL_NVP(_surviveForSeconds),
 			);
 	}
 };
@@ -29,6 +35,7 @@ struct LevelBinary
 	int _tileMapSizeX = 0;
 	int _tileMapSizeZ = 0;
 	std::vector<std::vector<int>> _gameObjectData = std::vector<std::vector<int>>();
+	std::vector<std::vector<int>> _enemyWavesData = std::vector<std::vector<int>>();
 
 	template<class A>
 	void serialize(A& a)
@@ -36,7 +43,8 @@ struct LevelBinary
 		a(
 			CEREAL_NVP(_tileMapSizeX),
 			CEREAL_NVP(_tileMapSizeZ),
-			CEREAL_NVP(_gameObjectData)
+			CEREAL_NVP(_gameObjectData),
+			CEREAL_NVP(_enemyWavesData)
 			);
 	}
 };
