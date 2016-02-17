b7d20882bb2d89810aa3ff2bfd1604e249c0b177
FredrikLindsten <ehh.va@hotmail.com>
2016-02-17 15:12:10 +0100

    ADDED hitboxes loaded from .bin files

--- StortSpelprojekt/System/CommonUtils.h
+++ StortSpelprojekt/System/CommonUtils.h
@@ -11,3 +11,8 @@ const std::wstring TEXTURE_FOLDER_PATH_W = L"Assets/Textures/";
 const std::string LEVEL_FOLDER_PATH = "Assets/Levels/Format12/";
 const std::string ANIMATION_FOLDER_PATH = "Assets/Animations/";
 const std::string BLUEPRINTS_PATH = "Assets/blueprints.json";
+
+struct Hitbox
+{
+	float _center[3], _height, _width, _depth;
+};
