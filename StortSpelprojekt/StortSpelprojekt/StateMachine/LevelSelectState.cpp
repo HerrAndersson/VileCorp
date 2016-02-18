fc22a9dfb9072a9431a14dcd7f5a85bf2ab61b5f
ze413X <alexander_grahn@hotmail.com>
2016-02-18 14:51:57 +0100

    ADDED Tutorialpage in level select

--- StortSpelprojekt/StortSpelprojekt/StateMachine/LevelSelectState.cpp
+++ StortSpelprojekt/StortSpelprojekt/StateMachine/LevelSelectState.cpp
@@ -5,13 +5,24 @@
 LevelSelectState::LevelSelectState(System::Controls* controls, ObjectHandler* objectHandler, System::Camera* camera, PickingDevice* pickingDevice, const std::string& filename, AssetManager* assetManager, FontWrapper* fontWrapper, System::SettingsReader* settingsReader, System::SoundModule* soundModule) :
 	BaseState(controls, objectHandler, camera, pickingDevice, filename, assetManager, fontWrapper, settingsReader, soundModule)
 {
-	_levelSelection = 7;
-	_levelSelectionMin = 7;
+	if (_settingsReader->GetProfile()->_firstTime)
+	{
+		_levelSelection = _levelSelection = 6;
+		_tutorial = true;
+	}
+	else
+	{
+		_levelSelection = 7;
+	}
+
+	
+	_levelSelectionMin = 6;
 	_levelSelectionMax = 9; //TODO: Add to this if more levels are added
-	for (int i = _levelSelectionMin+1; i < _levelSelectionMax + 1; i++)
+	for (int i = _levelSelectionMin; i < _levelSelectionMax + 1; i++)
 	{
 		_uiTree.GetNode(std::to_string(i))->SetHidden(true);
 	}
+	_uiTree.GetNode(std::to_string(_levelSelection))->SetHidden(false);
 }
 
 
@@ -34,7 +45,7 @@ void LevelSelectState::Update(float deltaTime)
 		System::MouseCoord coord = _controls->GetMouseCoord();
 		if (_uiTree.IsButtonColliding("playbutton", coord._pos.x, coord._pos.y))
 		{
-			_soundModule->Play("Assets/Sounds/page");
+			_soundModule->Play("Assets/Sounds/page.wav");
 			_objectHandler->LoadLevel(_levelSelection);
 			ChangeState(State::PLACEMENTSTATE);
 		}
@@ -42,7 +53,7 @@ void LevelSelectState::Update(float deltaTime)
 		{
 			if (_levelSelection > _levelSelectionMin)
 			{
-				_soundModule->Play("Assets/Sounds/page");
+				_soundModule->Play("Assets/Sounds/page.wav");
 				_uiTree.GetNode(std::to_string(_levelSelection))->SetHidden(true);
 				_levelSelection--;
 				_uiTree.GetNode(std::to_string(_levelSelection))->SetHidden(false);
@@ -52,7 +63,7 @@ void LevelSelectState::Update(float deltaTime)
 		{
 			if (_levelSelection < _levelSelectionMax)
 			{
-				_soundModule->Play("Assets/Sounds/page");
+				_soundModule->Play("Assets/Sounds/page.wav");
 				_uiTree.GetNode(std::to_string(_levelSelection))->SetHidden(true);
 				_levelSelection++;
 				_uiTree.GetNode(std::to_string(_levelSelection))->SetHidden(false);
@@ -64,7 +75,7 @@ void LevelSelectState::Update(float deltaTime)
 void LevelSelectState::OnStateEnter()
 {
 	//TODO: This is hardcoded to nine due to the functionality in LevelSelectState constructor /Sebastian
-	_levelSelectionMax = min(_levelSelectionMin + _settingsReader->GetProfile()->_level, 9);
+	//_levelSelectionMax = min(_levelSelectionMin + _settingsReader->GetProfile()->_level, 9);
 }
 
 void LevelSelectState::OnStateExit()
