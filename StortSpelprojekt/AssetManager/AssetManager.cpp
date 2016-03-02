54d2a7bab55d476cdcc74cc1e490943925ee6966
rikhardsvensson <rille88@gmail.com>
2016-03-02 11:23:39 +0100

    FIXED Texture loading errors in AssetManager

--- StortSpelprojekt/AssetManager/AssetManager.cpp
+++ StortSpelprojekt/AssetManager/AssetManager.cpp
@@ -68,18 +68,21 @@ void AssetManager::Flush()
 HRESULT Texture::LoadTexture(ID3D11Device* device)
 {
 	HRESULT res = S_OK;
+
 	if (!_loaded)
 	{
 		wstring _filePath = System::TEXTURE_FOLDER_PATH_W;
 		_filePath.append(_name.begin(), _name.end());
-//		res = DirectX::CreateWICTextureFromFile(device, _filePath.c_str(), nullptr, &_data, 0);
-//		DirectX::CreateDDSTextureFromFile(device, (_filePath.substr(0,_filePath.size()-3) + L"dds").c_str(), nullptr, &_data, 0);
-		res = DirectX::CreateDDSTextureFromFileEx(device, _filePath.c_str(), 0, D3D11_USAGE_IMMUTABLE, D3D11_BIND_SHADER_RESOURCE, 0, 0, false, nullptr, &_data,0);
+		//		res = DirectX::CreateWICTextureFromFile(device, _filePath.c_str(), nullptr, &_data, 0);
+		//		DirectX::CreateDDSTextureFromFile(device, (_filePath.substr(0,_filePath.size()-3) + L"dds").c_str(), nullptr, &_data, 0);
+		res = DirectX::CreateDDSTextureFromFileEx(device, _filePath.c_str(), 0, D3D11_USAGE_IMMUTABLE, D3D11_BIND_SHADER_RESOURCE, 0, 0, false, nullptr, &_data, 0);
 
-		if (_data == nullptr)
+		if (res != S_OK)
 		{
- 			string filenameString(_filePath.begin(), _filePath.end());
-			throw std::runtime_error("Texture " + filenameString + " not found");
+			string filenameString(_filePath.begin(), _filePath.end());
+			std::string errorMessage = "On Load Texture: " + _name + ",";
+			errorMessage += res;
+			throw std::runtime_error(errorMessage);
 		}
 		_loaded = true;
 	}
@@ -94,7 +97,8 @@ bool Mesh::DecrementUsers()
 }
 
 //Loads a model to the GPU
-bool AssetManager::LoadModel(string name, Mesh* mesh) {
+bool AssetManager::LoadModel(string name, Mesh* mesh)
+{
 
 	string file_path = System::MODEL_FOLDER_PATH;
 
@@ -110,7 +114,7 @@ bool AssetManager::LoadModel(string name, Mesh* mesh) {
 	vector<WeightedVertex> weightedVertices;
 
 	_infile->seekg(mesh->_toMesh);
-	if(mesh->_isSkinned)
+	if (mesh->_isSkinned)
 	{
 		weightedVertices.resize(mesh->_vertexBufferSize);
 		_infile->read((char*)weightedVertices.data(), mesh->_vertexBufferSize*sizeof(WeightedVertex));
@@ -129,9 +133,14 @@ bool AssetManager::LoadModel(string name, Mesh* mesh) {
 
 Texture* AssetManager::ScanTexture(string name)
 {
+	if (name.empty())
+	{
+		throw exception("ScanTexture: Name is empty");
+	}
+
 	Texture* texture = new Texture;
 
-	if (name.substr(name.size() - 4) == string(".png"))//TODO remove - Fredrik
+	if (name.find(".png") != std::string::npos)
 	{
 		name.resize(name.size() - 4);
 	}
@@ -167,27 +176,27 @@ Mesh* AssetManager::ScanModel(string name)
 	mesh->_name = name;
 
 	_infile->close();
-/*
-	MatHeader matHeader;
-	_infile->read((char*)&matHeader, sizeof(MatHeader));
-	_infile->read((char*)&renderObject->_diffuse, 16);
-	_infile->read((char*)&renderObject->_specular, 16);
+	/*
+		MatHeader matHeader;
+		_infile->read((char*)&matHeader, sizeof(MatHeader));
+		_infile->read((char*)&renderObject->_diffuse, 16);
+		_infile->read((char*)&renderObject->_specular, 16);
 
-	string diffFile, specFile;
-	diffFile.resize(matHeader._diffuseNameLength);
-	specFile.resize(matHeader._specularNameLength);
-	_infile->read((char*)diffFile.data(), matHeader._diffuseNameLength);
-	_infile->read((char*)specFile.data(), matHeader._specularNameLength);
+		string diffFile, specFile;
+		diffFile.resize(matHeader._diffuseNameLength);
+		specFile.resize(matHeader._specularNameLength);
+		_infile->read((char*)diffFile.data(), matHeader._diffuseNameLength);
+		_infile->read((char*)specFile.data(), matHeader._specularNameLength);
 
-	if (matHeader._diffuseNameLength)
-	{
-		renderObject->_diffuseTexture = ScanTexture(diffFile);
-	}
-	if (matHeader._specularNameLength)
-	{
-		renderObject->_specularTexture = ScanTexture(specFile);
-	}
-	*/
+		if (matHeader._diffuseNameLength)
+		{
+			renderObject->_diffuseTexture = ScanTexture(diffFile);
+		}
+		if (matHeader._specularNameLength)
+		{
+			renderObject->_specularTexture = ScanTexture(specFile);
+		}
+		*/
 
 	if (mesh->_isSkinned)
 	{
@@ -647,7 +656,7 @@ Skeleton* AssetManager::LoadSkeleton(string name)
 			skeleton->_actions[a]._bones[b]._frameTime.resize(frames);
 			_infile->read((char*)skeleton->_actions[a]._bones[b]._frameTime.data(), frames * sizeof(int));
 			skeleton->_actions[a]._bones[b]._frames = (Frame*)_aligned_malloc(sizeof(Frame) * frames, 16);
-			
+
 			for (int i = 0; i < frames; i++)
 			{
 				_infile->read((char*)&translation, sizeof(XMFLOAT3));
